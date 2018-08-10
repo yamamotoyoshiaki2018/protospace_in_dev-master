dcb42b05a452462b63218ae5d2e240c01fc02642
seiji <ymst500@gmail.com>
2018-08-10 10:28:57 +0900

    Edit comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
-    redirect_to "/comments/#{@comment.prototype.id}"
+    @comment = Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
+    redirect_to "/prototypes/#{@comment.prototype.id}"
   end
 
   private

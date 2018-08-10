2fe158607b5a2f914d32b27453107f214869ec80
seiji <ymst500@gmail.com>
2018-08-10 09:59:36 +0900

    Definition of create action on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :prototype_id)
+  end
 end

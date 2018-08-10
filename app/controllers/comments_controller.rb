e3116c130ea8004bf689087b74b5686cedaa6d5b
seiji <ymst500@gmail.com>
2018-08-10 10:02:02 +0900

    def create on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
+    redirect_to "/comments/#{@comment.prototype.id}"
   end
 
   private

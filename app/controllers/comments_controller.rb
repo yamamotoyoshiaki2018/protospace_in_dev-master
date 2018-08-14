f5fdc81f26637c1a5fcaeedbd5ae18aafab91129
seiji <ymst500@gmail.com>
2018-08-14 12:22:47 +0900

    Add comment delete
    非同期がうまくいかない

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,16 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      respond_to do |format|
+        format.html { redirect_to prototype_path(params[:prototype_id])  }
+        format.json
+      end
+    end
+  end
+
   private
   def comment_params
     params.permit(:text, :prototype_id)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :prototype
+  validates :text, presence: true
 end

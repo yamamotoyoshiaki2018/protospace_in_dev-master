3bb220f627e452d0343aff3f9212f78d9dbcef93
seiji <ymst500@gmail.com>
2018-08-20 09:39:24 +0900

    heroku-error-fix
    Production.rb edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # 上記を下記に修正
+  config.serve_static_assets = true
   # Compress JavaScripts and CSS. コメントアウト
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
